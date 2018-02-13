5fae4442a65d9fae89212ebe343f76c4e8237cd4
Edy Segura <ednardo@inatel.br>
2018-02-13 16:12:21 -0200

    Clean up

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,35 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
-import { StarWarsService } from '../star-wars.service';
-
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.css']
 })
 export class TabsComponent implements OnInit {
-  characters = [];
-  chosenSide = 'all';
 
-  swService: StarWarsService;
-
-  constructor(swService: StarWarsService) {
-    this.swService = swService;
-   }
+  constructor() {}
 
   ngOnInit() {
   }
-
-  onChoose(side) {
-    this.chosenSide = side;
-  }
-
-  isActive(side): boolean {
-    return side === this.chosenSide;
-  }
-
-  getCharacters() {
-    return this.swService.getCharacters(this.chosenSide);
-  }
-
 }
